b8b185fb97abf5902cea40ed5528019a11ad3d28
Gabriel Zuin <gabrielzuinj@gmail.com>
2022-10-30 23:40:43 -0300

    início localstorage e consulta API

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-const d = document.querySelector('.darker');
-const div = document.querySelector('.cd');
+const d = document.querySelector('.darker')
+const div = document.querySelector('.cd')
 // const div2 = document.querySelector('.cd');
-const containerForm = document.querySelector('#container-form');
+const containerForm = document.querySelector('#container-form')
 // tst = document.getElementsByClassName('.test');
 // div.addEventListener('click', function(){
 //     containerForm.style.display = 'none';
@@ -11,49 +11,85 @@ const containerForm = document.querySelector('#container-form');
 // });
 div.addEventListener('click', function () {
     if (containerForm.className == 'test-none' && d.className == 'darker') {
-        containerForm.className = 'test';
+        containerForm.className = 'test'
         d.className = 'darker-none'
     } else {
-        containerForm.className = 'test-none';
+        containerForm.className = 'test-none'
         d.className = 'darker'
     }
 });
 
 // form de login
 
-const inputEmail = document.querySelector('#mail');
-const inputPassword = document.querySelector('#pass');
-const submitButton = document.querySelector('.b-login');
+const inputEmail = document.querySelector('#mail')
+const inputPassword = document.querySelector('#pass')
+const submitButton = document.querySelector('.b-login')
 
-const candidate = document.querySelector('.person');
-const welcome = document.querySelector('.person2');
+const candidate = document.querySelector('.person')
+const welcome = document.querySelector('.person2')
+
+const search = document.querySelector('#pt2')
 
 submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const email = inputEmail.value;
-    const password = inputPassword.value;
-    
+    event.preventDefault()
+     
+    const email = inputEmail.value
+    const password = inputPassword.value
 
     fetch("https://reqres.in/api/login", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json"},
         body: JSON.stringify({ email, password })
     })
     .then(response => response.json())
     .then(data => {
         console.log(data)
         // candidate.className = "person-none"
-        console.log(candidate.textContent = "Olá, bem-vindo");
-    })
-    
-    
+        if(data.token != ''){
+            candidate.textContent = "Olá, bem-vindo"
+        }
+        containerForm.style.display = "none"
+        search.style.display = 'none'
+    }) 
 })
 
+const searchButton = document.querySelector('#searchBtn')
 
-// var request = new XMLHttpRequest();
-// request.open('GET', 'https://reqres.in/', true);
-// request.onload = function (){
-//     console.log(request.responseText);
-// };
-// request.send();
+// searchButton.addEventListener('click', () =>{
+//     async function loadPost() {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+//         const json = await response.json()
+//         const post = json.slice(0, 10)
+    
+//         post.forEach(post => {
+//             let div = document.createElement('#postContainer')
+//             div.classList.add('card-item')
+//             div.innerHTML = `
+//             <h3>${post.title}</h3>
+//             <p>${post.body}</p>
+//           `
+//           postscontainer.appendChild(div)
+//         });
+//     }
+//     loadPost('https://jsonplaceholder.typicode.com/posts')    
+// })
+
+async function getPost() {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const postscontainer = document.querySelector('#postContainer')
+    postscontainer.innerHTML = `<h3>${response.url}</h3>`
+    postscontainer.innerHTML = `<p>${response.url}</p>`
+
+}
+
+localStorage.setItem('email', 'eve.holt@reqres.in')
+
+
+
+
+
+
+
+
